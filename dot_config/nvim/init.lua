a43e1ebb0c996ec6232351ee906c192ce3aab400
Andrew Morger <asmorger@live.com>
2023-07-27 15:39:02 -0500

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Update .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/plugins/git.lua Add .config/nvim/lua/plugins/theme.lua Add .config/nvim/stylua.toml

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,10 +1,2 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.cmd('source ~/.config/nvim/old.vim')
-
-require("options")
-require("plugins")
-require("plugin-config")
-require("autocommands")
-require("keybindings")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
