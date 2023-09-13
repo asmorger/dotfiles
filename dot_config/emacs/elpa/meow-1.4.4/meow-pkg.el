c9de0d7cda0c5c050e00ce9911010f81add8e788
Andrew Morger <asmorger@live.com>
2023-09-13 15:47:44 -0500

    Cleans up emacs config

--- dot_config/emacs/elpa/meow-1.4.4/meow-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from meow.el  -*- no-byte-compile: t -*-
-(define-package "meow" "1.4.4" "Yet Another modal editing" '((emacs "27.1")) :commit "f553db183a47b23d037b1a7f1cab48b0b4798a37" :keywords '("convenience" "modal-editing") :url "https://www.github.com/DogLooksGood/meow")
