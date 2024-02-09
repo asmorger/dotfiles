4ffd6ecaaa126585571202972a54fd87cfa31b3f
Andrew Morger <asmorger@live.com>
2024-02-09 06:46:16 -0600

    First running example of nix home-manager

--- flake.nix
+++ flake.nix
@@ -2,22 +2,24 @@
   description = "Hawkbane's Home Manager flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+      nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+      home-manager = {
+          url = "github:nix-community/home-manager";
+          inputs.nixpkgs.follows = "nixpkgs";
+      };
   };
 
-  outputs = inputs: {
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
- 
-    homeConfigurations = {
-      "asmorger" = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux "
-        homeDirectory = "/home/asmorger";
-        username = "asmorger";
-        configuration.imports = [ ./home.nix ];
+  outputs = {nixpkgs, home-manager, ...}: {
+      # For `nix run .` later
+      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+
+      homeConfigurations = {
+          "hawkbane" = home-manager.lib.homeManagerConfiguration {
+              # Note: I am sure this could be done better with flake-utils or something
+              pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+              modules = [ ./home.nix ]; # Defined later
+          };
       };
-    };
   };
 }
