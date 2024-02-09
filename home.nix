228e70d6d4509eb7ed4f37b8f40eac46a89a7fc2
Andrew Morger <asmorger@live.com>
2024-02-09 07:20:07 -0600

    Adds dotnet and system commands

--- home.nix
+++ home.nix
@@ -17,7 +17,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -35,10 +35,15 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
 
-    pkgs.hello
-    pkgs.cowsay
-    pkgs.nixpkgs-fmt
-    pkgs.fzf
+    # languages
+    dotnet-sdk_8
+
+    # system tools
+    nixpkgs-fmt
+    fzf
+    bat
+    bottom
+    eza
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
