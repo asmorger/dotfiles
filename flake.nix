e7dcd7791f804fdcd34866909f8ea2eff6bacfb5
Andrew Morger <asmorger@live.com>
2024-02-09 06:34:08 -0600

    Adds initial nix flake files

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Hawkbane's Home Manager flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs: {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+ 
+    homeConfigurations = {
+      "asmorger" = inputs.home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-linux "
+        homeDirectory = "/home/asmorger";
+        username = "asmorger";
+        configuration.imports = [ ./home.nix ];
+      };
+    };
+  };
+}
